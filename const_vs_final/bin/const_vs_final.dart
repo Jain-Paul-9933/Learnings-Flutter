7b19ca003829e51ba4367832e99596092170b5a0
Jain-Paul-9933 <japs9933@gmail.com>
2024-06-23 23:13:00 +0530

    Improvement/Learned Const and Final

---
+++ const_vs_final/bin/const_vs_final.dart
@@ -0,0 +1,10 @@
+void main(List<String> arguments) {
+  // final String name;
+  // name = 'some name';
+
+  // final List<int> numberList;
+  // numberList = List.empty();
+  // numberList.add(10);
+  // numberList.add(100);
+  const String name='Some name';
+}
