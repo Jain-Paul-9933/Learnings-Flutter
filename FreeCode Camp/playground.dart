912dce76479a219ce95713267c115cd82c87ceb0
Jain-Paul-9933 <japs9933@gmail.com>
2024-06-26 23:56:38 +0530

    Improvement/Learned Basic Input and Ouput,Data types,String,Type Conversion, Constant, null object

---
+++ FreeCode Camp/playground.dart
@@ -0,0 +1,111 @@
+// import 'dart:io';
+
+main() {
+  //Hello World
+
+  // print("hello world");
+  // var firstName = "Jain";
+  // String lastName = "Paul";
+
+  // print(firstName + " " + lastName);
+
+  //Input and Output
+
+  // stdout.writeln("What is your name ?");
+  // String? name = stdin.readLineSync();
+  // print('My name is $name');
+
+  //Data types
+
+  // int amount1 = 100;
+  // var amount2 = 200;
+
+  // print('Amount1: $amount1 | Amount2: $amount2 \n');
+
+  // String name1 = "Jain";
+  // var name2 = "Paul";
+
+  // print('My name is : $name1 $name2 \n');
+
+  // bool isItTrue1 = true;
+  // var isItTrue2 = false;
+
+  // print('isItTrue1: $isItTrue1 | isItTrue2: $isItTrue2 \n');
+
+  // dynamic weakVairable = 100;
+
+  // print('WeakVairable 1: $weakVairable \n');
+
+  // weakVairable = "Dart Programming";
+
+  // print('WeakVairable 2: $weakVairable \n');
+
+  // weakVairable = null;
+  // print(weakVairable);
+
+  //String,Type Conversion, Constant, null
+
+  // var s1 = 'Single quotes work well for string literals.';
+  // var s2 = 'Double quotes work just as well.';
+  // var s3 = 'It\'s easy to escape the string delimiter';
+  // var s4 = "It's even easier to use the other delimiter";
+
+  // print(s1);
+  // print(s2);
+  // print(s3);
+  // print(s4);
+  // print('');
+
+  // var s = r'In a raw string, not even \n gets special treatment';
+  // print(s);
+
+  // var age = 35;
+  // var str = "My age is : $age";
+  // print(str);
+
+//   var s1 = '''
+// You can create
+// multi-line strings like this one.
+// ''';
+
+//   var s2 = """
+// This is also a
+// multi-line string.
+// """;
+
+//   print(s1);
+//   print(s2);
+
+// String -> int
+  // var one = int.parse('1');
+  // assert(one == 1);
+
+  // //String -> double
+  // var onePointOne = double.parse('1.1');
+  // assert(onePointOne == 1.1);
+
+  // // int -> String
+  // String oneAsString = 1.toString();
+  // assert(oneAsString == '1');
+
+  // //double -> String
+  // String piAsString = 3.14159.toStringAsFixed(2);
+  // assert(piAsString == "3.14159");
+
+//   const aConstNum = 0;
+//   const aConstBool = true;
+//   const aConstString = 'a constant string';
+
+//   print(aConstNum);
+//   print(aConstBool);
+//   print(aConstString);
+
+//   print(aConstNum.runtimeType);
+//   print(aConstBool.runtimeType);
+//   print(aConstString.runtimeType);
+
+  // int? num = null;
+  // print(num);
+
+  
+}
