fe6ed1885005a33d9d6cbaa58560a87df72b352b
Jain-Paul-9933 <japs9933@gmail.com>
2024-06-28 23:53:16 +0530

    Improvement/Learned Loops

--- FreeCode Camp/playground.dart
+++ FreeCode Camp/playground.dart
@@ -188,18 +188,20 @@ main() {
   //   print('Confused');
   // }
 
-// Switch Statement
+// // Switch Statement
 
-  int number = 0;
+//   int number = 0;
 
-  switch (number) {
-    case 0:
-      print('Even');
-      break;
-    case 1:
-      print('Odd');
-      break;
-    default:
-      print("Confused");
-  }
+//   switch (number) {
+//     case 0:
+//       print('Even');
+//       break;
+//     case 1:
+//       print('Odd');
+//       break;
+//     default:
+//       print("Confused");
+//   }
+
+// Loops
 }
