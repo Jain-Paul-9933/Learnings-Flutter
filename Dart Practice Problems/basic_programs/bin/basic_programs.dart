26c6933f4df4b3ae21615f16255673bec5891dbb
Jain-Paul-9933 <japs9933@gmail.com>
2024-07-03 23:58:58 +0530

    Improvement/Started to do basic programs with dart

---
+++ Dart Practice Problems/basic_programs/bin/basic_programs.dart
@@ -0,0 +1,5 @@
+import 'package:basic_programs/basic_programs.dart' as basic_programs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${basic_programs.calculate()}!');
+}
---
+++ Dart Practice Problems/basic_programs/test/basic_programs_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_programs/basic_programs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
